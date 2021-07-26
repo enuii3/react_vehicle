60e643b35498e7f5dc1e6398e088f7ae8dcfe8d3
enuii3 <b.nxxxi551@gmail.com>
2021-07-27 04:20:14 +0900

    fix: タイポの修正

--- src/app/store.js
+++ src/app/store.js
@@ -5,6 +5,6 @@ import vehicleReducer from "../features/vehicleSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
-    vehilce: vehicleReducer,
+    vehicle: vehicleReducer,
   },
 });
